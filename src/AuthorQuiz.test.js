a3bf39929a3dd1937429b1a76a1c3203d4036d9e
Charles King <charlesking@Charless-iMac.local>
2018-08-05 20:57:25 -0500

    AuthorQuiz boottrap

---
+++ src/AuthorQuiz.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthorQuiz from './AuthorQuiz';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AuthorQuiz />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe("When settingup testing",()=>{
+  it("should fail",()=>{
+    expect(1+1).toBe(3);
+  });
+})
