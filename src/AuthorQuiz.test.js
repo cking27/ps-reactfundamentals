9a624718a6adb5e16acf2094d5ec9dcf05bd687c
Charles King <charlesking@Charless-iMac.local>
2018-08-14 08:14:53 -0500

    Your Message

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AuthorQuiz />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Author Quiz", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<AuthorQuiz />, div);
+    });
 });
-
-
-describe("When settingup testing",()=>{
-  it("should fail",()=>{
-    expect(1+1).toBe(3);
-  });
-})
