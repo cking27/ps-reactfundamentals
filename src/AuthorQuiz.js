2616468bdbad8eea379c3f4cd8b6fb94d18706f8
Charles King <charlesking@Charless-iMac.local>
2018-09-06 20:15:23 -0500

    Code after events ,

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
 
@@ -11,12 +12,14 @@ function Hero() {
   </div>);
 }
 
-function Book({title}) {
-  return (<div className="answer">
+
+function Book({title, onClick}) {
+  return (<div className="answer" onClick={() => {onClick(title);}}>
     <h4>{title}</h4>
   </div>
   );
 }
+
 
 function Turn({author, books, highlight,onAnswerSelected}) {
 
@@ -40,6 +43,17 @@ function Turn({author, books, highlight,onAnswerSelected}) {
     </div>
   </div>);
 }
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
 
 function Continue() {
   return (<div/>);
