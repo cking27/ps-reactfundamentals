8caf86eff9743e682751cb585a83452ac2346d68
Charles King <charlesking@Charless-iMac.local>
2018-09-09 08:47:30 -0500

    Your Message

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
@@ -12,7 +13,6 @@ function Hero() {
   </div>);
 }
 
-
 function Book({title, onClick}) {
   return (<div className="answer" onClick={() => {onClick(title);}}>
     <h4>{title}</h4>
@@ -20,26 +20,22 @@ function Book({title, onClick}) {
   );
 }
 
-
-function Turn({author, books, highlight,onAnswerSelected}) {
-
-  function highlightToBgColor(){
-
-      const mapping = {
-        'none':'',
-        'correct':'green',
-        'wrong':'red'
-      }
+function Turn({author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
     return mapping[highlight];
   }
-
 
   return (<div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
     <div className="col-4 offset-1">
       <img src={author.imageUrl} className="authorimage" alt="Author"/>
     </div>
     <div className="col-6">
-      {books.map((title) => <Book title={title} key={title}  onClick={onAnswerSelected}  />)}
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
     </div>
   </div>);
 }
@@ -55,8 +51,16 @@ Turn.propTypes = {
   highlight: PropTypes.string.isRequired
 };
 
-function Continue() {
-  return (<div/>);
+function Continue({ show, onContinue }) {
+  return (
+    <div className="row continue">
+    { show 
+      ? <div className="col-11">
+          <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+        </div>
+      : null }
+    </div>
+  );
 }
 
 function Footer() {
@@ -69,12 +73,13 @@ function Footer() {
   </div>);
 }
 
-function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+function AuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
   return (
     <div className="container-fluid">
       <Hero />
       <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
-      <Continue />
+      <Continue show={highlight === 'correct'} onContinue={onContinue}/>
+      <p><Link to="/add">Add an author</Link></p>
       <Footer />
     </div>
   );
